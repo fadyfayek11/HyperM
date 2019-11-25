3d011fc3fccafb0101ef2574627aa9595a4e2a46
mohra <mohra@DESKTOP-OS5FVBI>
2019-11-26 01:08:35 +0200

    Inventory class

---
+++ Hyper/src/hyper/Marketing.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hyper;
+
+/**
+ *
+ * @author mohra
+ */
+public class Marketing extends Products {
+
+    //default constractor
+    public Marketing() {
+    }
+    
+    public void SetReports(){
+        
+    }
+    
+    public void Discount(){
+        
+    }
+    
+}
