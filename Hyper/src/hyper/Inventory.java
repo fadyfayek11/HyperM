5eb4f64b65ecb6ed1e4be71e44303fd2195092d7
mohra <mohra@DESKTOP-OS5FVBI>
2019-11-26 00:36:54 +0200

    Inventory class

---
+++ Hyper/src/hyper/Inventory.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hyper;
+
+/**
+ *
+ * @author mohra
+ */
+public class Inventory extends Products{
+    
+    //defult constractor
+    public Inventory(){
+        
+    }
+    
+         //add product
+    public void AddProducts(int date, String name ,int productID , int amount , double productPrice){
+        //will be done after making the database or files
+    }
+    
+         //remove product
+    public void DeleteProduct(String productname , int productid){
+        //will be done after making the database or files
+    }
+              
+          //get message when expiry date of product get close
+    public void ExpirationWarning(int date , String product){
+           //will be done after making the database or files
+    }
+     
+           //get message when product is lacking
+    public void ShortageWarn(int amount , String productname){
+           //will be done after making the database or files
+    }
+    
+}
