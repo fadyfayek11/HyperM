aa0aae201a57cf1fbf47f20a8efadf6be63e5046
Fady Fayek <Fady Fayek@192.168.1.112>
2019-11-26 15:44:37 +0200

    Admin_calss

---
+++ Hyper/src/hyper/AdminClass.java
@@ -0,0 +1,74 @@
+
+package hyper;
+
+
+public class AdminClass extends EmployeeClass{
+    
+    
+    
+  /*main constractor*/  
+public  AdminClass(int id_of_employee, String phonenumber, String password, String name, String type)    
+{
+        super(id_of_employee, phonenumber, password, name, type);
+       
+}
+
+
+/*empty constractor*/
+    public AdminClass() {  
+    }
+
+    /* main function of admin*/
+
+
+
+
+
+//ADD employee
+    public void AddEmployee(int id_of_employee, String phonenumber, String password, String name, String type) {
+        //files work
+        super.setName(name);
+        super.setNumber(phonenumber);
+        super.setPassword(password);
+        super.setType(type);
+        super.setId_of_employee(id_of_employee);
+        Updatelist();
+    }
+    
+    
+    //Delete
+public  void DeleteEmployee(int id_of_employee)
+{
+    //files work
+    Updatelist();
+}
+
+
+
+//Update
+public static void Updatelist()
+{
+    //files work with gui
+    //will show hint "updatet after every fun"
+    System.out.println("updated");
+}
+
+
+
+
+//Searsh
+public static int SearchEmployee(int id_of_employee)
+{
+    /*if id in file */
+    return 1;
+    /*else 
+    return 0;*/
+}
+
+
+//list
+public static void ListofEmployee()
+{
+    //files
+}
+}
