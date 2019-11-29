24fbb1dbb2fc3814cfc9891405b637623184575b
Fady Fayek <Fady Fayek@192.168.1.112>
2019-11-29 16:53:26 +0200

    new

--- Hyper/src/hyper/EmployeeClass.java
+++ Hyper/src/hyper/EmployeeClass.java
@@ -19,51 +19,45 @@ public class EmployeeClass {
    private int id_of_employee;
 
    
-/* main constractor*/
-    public EmployeeClass(int id_of_employee,String phonenumber, String password, String name, String type) {
-      
-       setName(name);
-       setId_of_employee(id_of_employee);
-       setType(type);
-       setNumber(phonenumber);
-       setPassword(password);
-       }
+    /* main constractor*/
+    public EmployeeClass(int id_of_employee, String phonenumber, String password, String name, String type) {
 
-   
-/*empty constructor*/
+        setName(name);
+        setId_of_employee(id_of_employee);
+        setType(type);
+        setNumber(phonenumber);
+        setPassword(password);
+    }
+
+    /*empty constructor*/
     public EmployeeClass() {
     }
-    
-/*phonenubmer of employee*/
+
+    /*phonenubmer of employee*/
     public String getNumber() {
         return phonenumber;
     }
-/*phone number*/
+
+    /*phone number*/
     public void setNumber(String phonenumber) {
-       if(phonenumber.length()<=11&&phonenumber.length()>10)
-        {
-            
+        if (phonenumber.length() <= 11 && phonenumber.length() > 10) {
+
             this.phonenumber = phonenumber;
+        } else {
+            System.out.println("incorrect phone number-should be 11 number");
         }
-        else
-        {
-           System.out.println("incorrect phone number-should be 11 number");
-        }
-      
-        
+
     }
-/*password of employee*/
+
+    /*password of employee*/
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
-        if(password.length()<8)
-        {
+        if (password.length() < 8) {
             System.out.println("the password should be more than 8 char");
-        }
-        else
-        {
+        } else {
             this.password = password;
         }
     }
@@ -71,54 +65,42 @@ public class EmployeeClass {
     public String getName() {
         return name;
     }
-/*name of employee*/
+
+    /*name of employee*/
     public void setName(String name) {
         this.name = name;
     }
-/*type of employee*/
+
+    /*type of employee*/
     public String getType() {
         return type;
     }
 
     public void setType(String type) {
-        if(type=="Marketing Employee")
-        {
+        if (type == "Marketing Employee") {
             this.type = type;
-        }
-        else if(type=="Inventory Employee")
-        {
+        } else if (type == "Inventory Employee") {
             this.type = type;
-        }
-        else if(type=="Seller Employee")
-        {
+        } else if (type == "Seller Employee") {
             this.type = type;
-        }
-        else if(type=="Admin")
-        {
+        } else if (type == "Admin") {
             this.type = type;
-        }
-        else
-        {
+        } else {
             System.out.println("please enter the type 'Admin-Marketing Employee-Inventory Employee-Seller Employee' ");
         }
-       }
-
-    
+    }
 
     public int getId_of_employee() {
         return id_of_employee;
     }
 
     public void setId_of_employee(int id_of_employee) {
-        try{
+        try {
             this.id_of_employee = id_of_employee;
-        }catch(RuntimeException e)
-        {
+        } catch (RuntimeException e) {
             System.out.println("please enter numbers only");
         }
-        
-    }
-   
 
-    
+    }
+
 }
